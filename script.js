1dbc66f814b910b306719aba04775ce82b61617a
Raton <82051324+raaaton@users.noreply.github.com>
2025-02-15 17:12:44 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,78 @@
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const errorEl = document.getElementById("error-el");
+const ulEl = document.getElementById("ul-el");
+const noTodoEl = document.getElementById("no-todo-yet");
+
+let todoItems = [];
+
+inputBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    addItemToList();
+});
+
+ulEl.addEventListener("click", function(e) {
+    if (e.target.classList.contains("delete-el")) {
+        const index = e.target.getAttribute('data-index');
+        deleteItem(index);
+    } else if (e.target.classList.contains("todo-checkbox")) {
+        const index = e.target.getAttribute('data-index');
+        toggleComplete(index);
+    }
+});
+
+function render() {
+    ulEl.innerHTML = "";
+    
+    // Séparer les tâches non complétées et complétées
+    let incompleteTodos = todoItems.filter(item => !item.completed);
+    let completeTodos = todoItems.filter(item => item.completed);
+    
+    // Concaténer les listes pour garder l'ordre de création
+    let sortedTodos = [...incompleteTodos, ...completeTodos];
+    
+    sortedTodos.forEach((item, index) => {
+        ulEl.innerHTML += `
+        <li>
+            <div class="todo-item">
+                <input type="checkbox" class="todo-checkbox" data-index="${todoItems.indexOf(item)}" ${item.completed ? "checked" : ""}>
+                ${item.text}
+            </div>
+            <span class="delete-el icon icon-bin" data-index="${todoItems.indexOf(item)}"></span>
+        </li>`;
+    });
+
+    if (todoItems.length === 0) {
+        noTodoEl.textContent = "You haven't created any Todo item yet. Try to create one.";
+    } else {
+        noTodoEl.textContent = "";
+    }
+}
+
+function addItemToList() {
+    const inputValue = inputEl.value;
+    if (inputValue === "") {
+        errorEl.textContent = "Please enter a valid value";
+    } else {
+        errorEl.textContent = "";
+        todoItems.push({ text: inputValue, completed: false });
+        inputEl.value = "";
+    }
+    render();
+}
+
+function deleteItem(index) {
+    index = parseInt(index);
+    if (!isNaN(index) && index >= 0 && index < todoItems.length) {
+        todoItems.splice(index, 1);
+        render();
+    }
+}
+
+function toggleComplete(index) {
+    index = parseInt(index);
+    if (!isNaN(index) && index >= 0 && index < todoItems.length) {
+        todoItems[index].completed = !todoItems[index].completed;
+        render();
+    }
+}
